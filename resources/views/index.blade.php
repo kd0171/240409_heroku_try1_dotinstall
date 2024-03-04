ab94eeb864b2d7b40c7aa8720ee4a4a9bb6331ff
kd0171 <kido.0117.1895@gmail.com>
2024-03-04 02:02:52 +0100

    bladeによるforeach構文 bladeによるforelse構文：データが空の場合の処理を@emptyの後に書ける

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,11 +9,24 @@
     <div class="container">
         <h1>My BBS</h1>
         <ul>
-            <li><?php echo htmlspecialchars($posts[0], ENT_QUOTES, 'UTF-8'); ?></li>
+            {{-- <li><?php echo htmlspecialchars($posts[0], ENT_QUOTES, 'UTF-8'); ?></li> --}}
             {{-- 二重カッコの中に入れると上と同じコードになる --}}
-            <li>{{ $posts[0] }}</li>
+            {{-- <li>{{ $posts[0] }}</li>
             <li>{{ $posts[1] }}</li>
-            <li>{{ $posts[2] }}</li>
+            <li>{{ $posts[2] }}</li> --}}
+
+            {{-- bladeによるforeach構文 --}}
+            @foreach ($posts as $post)
+                <li>{{ $post }}</li>
+            @endforeach
+
+            {{-- bladeによるforelse構文：データが空の場合の処理を@emptyの後に書ける --}}
+            @forelse ($posts as $post)
+                <li>{{ $post }}</li>
+            @empty
+                <li>No posts yet!</li>
+            @endforelse
+
             <li>Title</li>
             <li>Title</li>
             <li>Title</li>
