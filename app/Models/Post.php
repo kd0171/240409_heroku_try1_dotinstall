ac3f96e35bbfccc598b38cea7e42e03fdf8b69b0
kd0171 <kido.0117.1895@gmail.com>
2024-03-05 17:04:41 +0100

    tinkerによるデータの挿入

---
+++ app/Models/Post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+// Post -> posts モデルの名前を Post としたことで、デフォルトだと posts テーブルに紐付けてくれるという仕組みになっています。
+class Post extends Model
+{
+    use HasFactory;
+}
