204a2d25b2f68c91671faf5802c4e9905789d8d5
kd0171 <kido.0117.1895@gmail.com>
2024-03-06 06:18:09 +0100

    削除機能と削除前チェック

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -14,6 +14,12 @@
     <h1>
         {{ $post->title}}
         <a href="{{ route('posts.edit', $post) }}">[Edit]</a>
+        <form method="post" action="{{ route('posts.destroy', $post) }}" id="delete_post">
+            @method('DELETE')
+            @csrf
+
+            <button class="btn">[x]</button>
+        </form>
     </h1>
 
 
@@ -26,4 +32,22 @@
     {{-- <p>{!! nl2br($post->body) !!}</p> --}}
     {{-- htmlspecialchars を使えば良いのですが、 Laravel では e() を使って短く書ける --}}
     <p>{!! nl2br(e($post->body)) !!}</p>
+
+
+    <script>
+        // 厳密なエラーチェック
+        'use strict';
+
+        {
+            document.getElementById('delete_post').addEventListener('submit', e => {
+                e.preventDefault();
+
+                if (!confirm('Sure to delete?')) {
+                    return;
+                }
+
+                e.target.submit();
+            });
+        }
+    </script>
 </x-layout>
